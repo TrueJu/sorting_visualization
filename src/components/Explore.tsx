95e21f9552fb9914e5f2651e3363c5c8447e3143
TrueJu <julien33widmer@gmail.com>
2022-11-10 18:03:18 +0100

    explore section

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -9,23 +9,7 @@ const catalogue = [
     },
     {
         "name": "Bubble sort",
-        "content": "test content blabla iofsdf"
-    },
-    {
-        "name": "Bubble sort",
-        "content": "test content blabla iofsdf"
-    },
-    {
-        "name": "Bubble sort",
-        "content": "test content blabla iofsdf"
-    },
-    {
-        "name": "Bubble sort",
-        "content": "test content blabla iofsdf"
-    },
-    {
-        "name": "Bubble sort",
-        "content": "test content blabla iofsdf"
+        "content": "Bubble sort, sometimes referred to as sinking sort. It repeatedly steps through the input list element by element, comparing the current element with the one after it, swapping their values if needed."
     }
 ];
 
@@ -48,12 +32,12 @@ type CardProps = {
     title: string,
     content: string
 }
-
+//1340px
 const Card = ({ img, title="default", content="default" }:CardProps) =>
-    <div className="shadow-xl shadow-[#ffffff08] overflow-hidden border-[1px] rounded-xl w-1/4 margin-between-cards p-6 pb-3 cursor-pointer hover:bg-[#ffffff03] group transition-all">
+    <div className="shadow-xl shadow-[#ffffff08] overflow-hidden border-[1px] rounded-xl xs:w-full ss:w-60 sm:w-72 xsm:w-96 md:w-[400px] xmd:w-80 p-6 pb-3 mt-5 cursor-pointer hover:bg-[#ffffff03] group transition-all">
         <div className="w-24 h-20 -ml-6 -mt-6 -mb-[56px] blur-2xl opacity-10 bg-blue-gradient"></div>
         <img src={img} alt="Sorting Icon" className="w-12 absolute" />
-        <h2 className="ml-24 text-2xl cursor-pointer text-blue-gradient">{title}</h2>
+        <h2 className="ml-24 text-2xl cursor-pointer text-blue-gradient font-bold">{title}</h2>
         <p className="mt-9 cursor-pointer">{content}</p>
         <hr className="card-hr" />
         <button className="pt-3 group-hover:font-bold group-hover:ml-[50%] group-hover:-translate-x-1/2 transition-all ease-out duration-500">Check it out!</button>
