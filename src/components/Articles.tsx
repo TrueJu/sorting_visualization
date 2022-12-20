4e78612a25112577f5663579fd00da6b627c54f9
TrueJu <julien33widmer@gmail.com>
2022-12-20 10:34:52 +0100

    finished skeletal structure and completed bubble sort example

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -7,6 +7,7 @@ type articleProps = {
     title: string,
     description: string,
     sourceCodes: Array<any>,
+    timeAndSpace: Array<string>,
     id: string,
     animationScript: string,
     animationElement: JSX.Element
@@ -46,7 +47,86 @@ console.log(sortedArray);  // Output: [1, 2, 3, 4, 5]`], ["Kotlin", "kotlin", `f
 }
 
 val sortedArray = bubbleSort(intArrayOf(5, 2, 1, 4, 3))
-println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`]],
+println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`], ["Java", "java", `import java.util.Arrays;
+...
+public static int[] bubbleSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+        for (int j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+int[] sortedArray = bubbleSort(new int[] {5, 2, 1, 4, 3});
+System.out.println(Arrays.toString(sortedArray));  // Output: [1, 2, 3, 4, 5]`], ["Python", "phython", `def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap the elements
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+
+sorted_array = bubble_sort([5, 2, 1, 4, 3])
+print(sorted_array)  # Output: [1, 2, 3, 4, 5]`], ["C#", "csharp", `public static int[] BubbleSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+int[] sortedArray = BubbleSort(new int[] { 5, 2, 1, 4, 3 });
+Console.WriteLine(string.Join(", ", sortedArray));  // Output: 1, 2, 3, 4, 5`], ["Rust", "rust", `fn bubble_sort(arr: &mut [i32]) {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - i - 1 {
+            if arr[j] > arr[j + 1] {
+                // Swap the elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+let mut sorted_array = [5, 2, 1, 4, 3];
+bubble_sort(&mut sorted_array);
+println!("{:?}", sorted_array);  // Output: [1, 2, 3, 4, 5]`], ["Perl", "perl", `sub bubble_sort {
+    my @arr = @_;
+    for my $i (0..$#arr) {
+        for my $j (0..$#arr - $i - 1) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                # Swap the elements
+                my $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+    return @arr;
+}
+
+my @sorted_array = bubble_sort(5, 2, 1, 4, 3);
+print "@sorted_array\\n";  # Output: 1 2 3 4 5`]],
+        "timeAndSpace": ["1", "n^2", "n^2", "n"],
         "id": "1",
         "animationScript": "bubblesort",
         "animationElement": <div className="mt-5 mb-5 flex gap-[10px] w-full justify-center">
@@ -64,6 +144,7 @@ println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`]],
         "title": "merge sort",
         "description": "test content 2",
         "sourceCodes": [["Javascript", "javascript", "var jhdf = 10;<br>run();"], ["Kotlin", "kotlin", "var jhdf = 33;<br>run();"], ["C#", "csharp", "var jhdf = 23;<br>run();"]],
+        "timeAndSpace": ["1", "n^2", "n^2", "n"],
         "id": "2",
         "animationScript": "bubblesort",
         animationElement: <div>animation el</div>
@@ -72,6 +153,7 @@ println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`]],
         "title": "merge sort quick",
         "description": "test content 3",
         "sourceCodes": [["Javascript", "javascript", "var jhdf = 10;<br>run();"], ["Kotlin", "kotlin", "var jhdf = 33;<br>run();"]],
+        "timeAndSpace": ["1", "n^2", "n^2", "n"],
         "id": "3",
         "animationScript": "bubblesort",
         animationElement: <div>animation el</div>
@@ -80,6 +162,7 @@ println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`]],
         "title": "heap sort",
         "description": "test content 4",
         "sourceCodes": [["Javascript", "javascript", "var jhdf = 10;<br>run();"], ["Kotlin", "kotlin", "var jhdf = 33;<br>run();"]],
+        "timeAndSpace": ["1", "n^2", "n^2", "n"],
         "id": "3",
         "animationScript": "bubblesort",
         animationElement: <div>animation el</div>
@@ -87,7 +170,7 @@ println(sortedArray.joinToString())  // Output: [1, 2, 3, 4, 5]`]],
 ];
 
 const Articles = () => {
-    const [visibleArticle, setVisibleArticle] = useState(<Article key={0} title={articles[0].title} description={articles[0].description} animationScript={articles[0].animationScript} sourceCodes={articles[0].sourceCodes} id={articles[0].id} animationElement={articles[0].animationElement} />);
+    const [visibleArticle, setVisibleArticle] = useState(<Article key={0} title={articles[0].title} description={articles[0].description} animationScript={articles[0].animationScript} sourceCodes={articles[0].sourceCodes} timeAndSpace={articles[0].timeAndSpace} id={articles[0].id} animationElement={articles[0].animationElement} />);
 
     return (
         <div className="flex justify-between items-start flex-col mt-20 w-full gap-14 p-3">
@@ -97,7 +180,7 @@ const Articles = () => {
                                             console.log(searchPhraseEvent.target.value.toLowerCase())
                                             for(let i=0;i<articles.length;i++) {
                                                 if(articles[i].title.toLowerCase().replace(' ', '').includes(searchPhraseEvent.target.value.toLowerCase().replace(' ', ''))) {
-                                                    setVisibleArticle(<Article key={i} title={articles[i].title} description={articles[i].description} animationScript={articles[i].animationScript} sourceCodes={articles[i].sourceCodes} id={articles[i].id} animationElement={articles[i].animationElement} />);
+                                                    setVisibleArticle(<Article key={i} title={articles[i].title} description={articles[i].description} animationScript={articles[i].animationScript} sourceCodes={articles[i].sourceCodes} timeAndSpace={articles[i].timeAndSpace} id={articles[i].id} animationElement={articles[i].animationElement} />);
                                                     i = articles.length;
                                                 }
                                             }
@@ -108,6 +191,8 @@ const Articles = () => {
         </div>
     )
 }
+
+let updateAnimation = false;
 
 function bubbleSort() {
     const good_check_color = "#00a716";
@@ -172,12 +257,20 @@ function bubbleSort() {
             bubbleSort();
         } else {
             //finished
+            document.getElementById("runAnimationBtn")?.classList.toggle("hidden");
+            document.getElementById("animationRunningBtn")?.classList.toggle("hidden");
+
+            updateAnimation = true;
+
+            for(let i=0;i<dataBlockElements.length;i++) {
+                dataBlockElements[i].style.backgroundColor = default_color;
+            }
         }
     }, (dataBlocks.length + 1) * 600);
 }
   
 
-const Article = ({ title="default", description="...", animationScript, sourceCodes=[], id, animationElement }:articleProps) => {
+const Article = ({ title="default", description="...", animationScript, sourceCodes=[], timeAndSpace, id, animationElement }:articleProps) => {
     const sourceCodeElements = [];
 
     for(let i=0;i<sourceCodes.length;i++) {
@@ -197,17 +290,40 @@ const Article = ({ title="default", description="...", animationScript, sourceCo
             <h2 className={`text-3xl cursor-pointer text-${Math.floor(Math.random() * (2 - 1 + 1) + 1) == 1 ? 'red' : 'blue'}-gradient font-bold w-fit mb-5`}>{title}</h2>
             <div>
                 {description}
-            </div>
+            </div><br/>
             {animationElement}
-            <button onClick={() => {
+            <button  onClick={() => {
                 switch(animationScript) {
                     case "bubblesort":
+                        document.getElementById("runAnimationBtn")?.classList.toggle("hidden");
+                        document.getElementById("animationRunningBtn")?.classList.toggle("hidden");
+
+                        if(updateAnimation) {
+                            const dataBlockElements = document.querySelectorAll<HTMLElement>(".data-blocks");
+
+                            for(let i=0;i<dataBlockElements.length;i++) {
+                                dataBlockElements[i].innerHTML = String(Math.floor(Math.random() * (9 - 0 + 1) + 0));
+                            }
+                        }
+
                         bubbleSort();
                         break;
                     default:
                         console.error("error");
                 }
-            }}>sort</button>
+            }} type="button" id="runAnimationBtn" className="ml-[50%] translate-x-[-50%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Run Animation
+                <svg aria-hidden="true" className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </button>
+            <button disabled type="button" id="animationRunningBtn" className="hidden ml-[50%] translate-x-[-50%] py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 items-center">
+                <svg aria-hidden="true" role="status" className="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+                </svg>
+                Animating...
+            </button><br/><br/><br/>
+            <p><b>Space complexity:&ensp;</b> {timeAndSpace[0]}<br/><b>Time complexity:&emsp;</b> {timeAndSpace[1]} (worst), {timeAndSpace[2]} (average), {timeAndSpace[3]} (best)</p>
+            <br/><br/><br/>
             <div>
                 {sourceCodeElements}
             </div>
--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -199,8 +199,8 @@ const Card = ({ img, title="default", content="default", animated=false }:CardPr
         document.getElementById("algorithmSearchInput").value = title;
 
         const changeEvent = new Event('input', { bubbles: true });
-        // @ts-expect-error
-        document.getElementById("algorithmSearchInput").dispatchEvent(changeEvent);
+       
+        document.getElementById("algorithmSearchInput")!.dispatchEvent(changeEvent);
     }} className="shadow-xl shadow-[#ffffff09] overflow-hidden border-[1px] rounded-xl xs:w-full ss:w-60 sm:w-72 xsm:w-96 md:w-[400px] xmd:w-80 p-6 pb-3 mt-5 cursor-pointer hover:bg-[#ffffff03] group transition-all">
         <div className="w-24 h-20 -ml-6 -mt-6 -mb-[56px] blur-2xl opacity-10 bg-blue-gradient"></div>
         <img src={img} alt="Sorting Icon" className="w-12 absolute" />
