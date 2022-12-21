ba04f055cb6d70e7b2d8e88c8a725dd5bd0acaeb
TrueJu <julien33widmer@gmail.com>
2022-12-21 12:52:16 +0100

    finished quicksort example

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -240,12 +240,9 @@ public class QuickSort {
 sorted_array = quicksort([5, 2, 1, 4, 3])
 print(sorted_array) # [1, 2, 3, 4, 5]`], ["C#", "csharp", `using System.Collections.Generic;
 
-public class QuickSort
-{
-    public static List<int> Quicksort(List<int> array)
-    {
-        if (array.Count <= 1)
-        {
+public class QuickSort {
+    public static List<int> Quicksort(List<int> array) {
+        if (array.Count <= 1) {
             return array;
         }
 
@@ -253,14 +250,12 @@ public class QuickSort
         var left = new List<int>();
         var right = new List<int>();
 
-        for (int i = 1; i < array.Count; i++)
-        {
+        for (int i = 1; i < array.Count; i++) {
             if (array[i] < pivot)
             {
                 left.Add(array[i]);
             }
-            else
-            {
+            else {
                 right.Add(array[i]);
             }
         }
@@ -273,8 +268,7 @@ public class QuickSort
         return result;
     }
 
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
         var sortedArray = Quicksort(new List<int> { 5, 2, 1, 4, 3 });
         System.Console.WriteLine(string.Join(", ", sortedArray)); // 1, 2, 3, 4, 5
     }
@@ -427,17 +421,17 @@ function bubbleSort() {
     }, (dataBlocks.length + 1) * 600);
 }
 
-function quickSort() {
-    const dataBlockElements = document.querySelectorAll<HTMLElement>(".data-blocks");
-    const dataBlocks:any = [0,1,2,3,4,5];
+async function quickSort() {
+    let dataBlockElements = document.querySelectorAll<HTMLElement>(".data-blocks");
+    let dataBlocks:any = [0,1,2,3,4,5];
 
     for(let i=0;i<dataBlockElements.length;i++) {
         dataBlocks[parseInt(`${dataBlockElements[i].dataset.pos}`)] = dataBlockElements[i];
     }
 
-    function partition(startIndex:number, endIndex:number) {
+    async function partition(startIndex:number, endIndex:number) {
         // select the last element as a pivot from the array
-        const pivot = arr[endIndex]  //parseInt(dataBlocks[endIndex].innerHTML); //
+        const pivot = parseInt(dataBlocks[endIndex].innerHTML);
       
         // elements smaller than the pivot element goes to the left of `pivotIndex`
         // elements greater than the pivot element goes to the right of `pivotIndex`
@@ -445,20 +439,82 @@ function quickSort() {
         let pivotIndex = startIndex;
       
         // if an element is less than or equal to the pivot, we will increase the 'pivotIndex' and we will place that element before the pivot.
-        for (let i = startIndex; i < endIndex; i++) {
-            if (arr[i] <= pivot) {
-                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-                pivotIndex++;
-            }
+        async function iPivotSwap() {
+            return new Promise(resolve => {
+                for (let i = startIndex; i < endIndex; i++) {
+                    setTimeout(async () => {
+                        dataBlocks[i].style.backgroundColor = currently_checking_color;
+                        dataBlocks[endIndex].style.backgroundColor = currently_checking_color;
+
+                        await new Promise(resolve => setTimeout(resolve, 400));
+
+                        if(parseInt(dataBlocks[i].innerHTML) <= pivot) {
+                            let dataBlocks1Transform = dataBlocks[i].dataset.translate;
+                            let dataBlocks1Pos = dataBlocks[i].dataset.pos;
+                            
+                            let dataBlocks2Transform = dataBlocks[pivotIndex].dataset.translate;
+                            let dataBlocks2Pos = dataBlocks[pivotIndex].dataset.pos;
+
+                            dataBlocks[i].style.transform = `translateX(${dataBlocks2Transform}px)`;
+                            dataBlocks[i].setAttribute("data-pos", `${dataBlocks2Pos}`);
+                            dataBlocks[i].setAttribute("data-translate", dataBlocks2Transform);
+
+                            dataBlocks[pivotIndex].style.transform = `translateX(${dataBlocks1Transform}px)`;
+                            dataBlocks[pivotIndex].setAttribute("data-pos", `${dataBlocks1Pos}`);
+                            dataBlocks[pivotIndex].setAttribute("data-translate", dataBlocks1Transform);
+
+                            dataBlockElements = document.querySelectorAll<HTMLElement>(".data-blocks");
+                            dataBlocks = [0,1,2,3,4,5];
+
+                            for(let i=0;i<dataBlockElements.length;i++) {
+                                dataBlocks[parseInt(`${dataBlockElements[i].dataset.pos}`)] = dataBlockElements[i];
+                            }
+
+                            dataBlocks[i].style.backgroundColor = good_check_color;
+                            dataBlocks[endIndex].style.backgroundColor = good_check_color;
+
+                            pivotIndex++;
+                        } else {
+                            dataBlocks[i].style.backgroundColor = default_color;
+                            dataBlocks[endIndex].style.backgroundColor = default_color;
+                        }
+
+                        if(i == endIndex -1 || i == endIndex ) { //loop endet
+                            resolve(true);
+                        }
+                    }, i * 600);
+                }
+            });
         }
-      
-        // swap `pivotIndex` with pivot element
-        [arr[pivotIndex], arr[endIndex]] = [arr[endIndex], arr[pivotIndex]];
+
+        await iPivotSwap();
+
+        // swap pivotIndex with pivot element
+        let dataBlocks1Transform = dataBlocks[pivotIndex].dataset.translate;
+        let dataBlocks1Pos = dataBlocks[pivotIndex].dataset.pos;
+        
+        let dataBlocks2Transform = dataBlocks[endIndex].dataset.translate;
+        let dataBlocks2Pos = dataBlocks[endIndex].dataset.pos;
+
+        dataBlocks[pivotIndex].style.transform = `translateX(${dataBlocks2Transform}px)`;
+        dataBlocks[pivotIndex].setAttribute("data-pos", `${dataBlocks2Pos}`);
+        dataBlocks[pivotIndex].setAttribute("data-translate", dataBlocks2Transform);
+
+        dataBlocks[endIndex].style.transform = `translateX(${dataBlocks1Transform}px)`;
+        dataBlocks[endIndex].setAttribute("data-pos", `${dataBlocks1Pos}`);
+        dataBlocks[endIndex].setAttribute("data-translate", dataBlocks1Transform);
+
+        dataBlockElements = document.querySelectorAll<HTMLElement>(".data-blocks");
+        dataBlocks = [0,1,2,3,4,5];
+
+        for(let i=0;i<dataBlockElements.length;i++) {
+            dataBlocks[parseInt(`${dataBlockElements[i].dataset.pos}`)] = dataBlockElements[i];
+        }
       
         return pivotIndex;
     }
       
-    function sub_quickSort() {
+    async function sub_quickSort() {
         //stack for storing start and end Index of the subarrays
         const s = [];
         const n = dataBlocks.length;
@@ -479,7 +535,7 @@ function quickSort() {
             s.pop();
         
             // partitioning the elements around pivot
-            const pivotIdx = partition(startIdx, endIdx);
+            const pivotIdx = await partition(startIdx, endIdx);
         
             //push subarray indices to stack which has elements smaller than the current pivot
             if (pivotIdx - 1 > startIdx) {
@@ -492,12 +548,8 @@ function quickSort() {
             }
         }
     }
-      
-    const arr = [5, 2, 1, 4, 3];
 
-    sub_quickSort();
-    
-    console.log(arr);
+    await sub_quickSort();
 
     document.getElementById("runAnimationBtn")?.classList.toggle("hidden");
     document.getElementById("animationRunningBtn")?.classList.toggle("hidden");
